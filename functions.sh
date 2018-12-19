053e832f95cd7825f53c1a3b178b4e34111b960b
Potjoe-97 <42873357+Potjoe-97@users.noreply.github.com>
2018-12-19 01:30:23 +0100

    Removing comments

--- functions.sh
+++ functions.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-# Here you can create functions which will be available from the commands file
-# You can also use here user variables defined in your config file
-# To avoid conflicts, name your function like this
-# pg_XX_myfunction () { }
-# pg for PluGin
-# XX is a short code for your plugin, ex: ww for Weather Wunderground
-# You can use translations provided in the language folders functions.sh
+
 pv_dflow () {
 local answer=$(curl -s https://api.dialogflow.com/v1/query?v=20150910 -H 'Content-Type: application/json' -H 'Authorization: Bearer '${dflow_api_key}'' -d '{"lang": "'"${language:0:2}"'","query": "'"$order"'","sessionId": "112784433176887711459"}' | jq -r ".result .fulfillment .speech")
 if [ -n "${answer}" ]; then
